78bd156300d4621fef2de3bf43be1479caf4225d
atharv1234 <atharvterwadkar@gmail.com>
2023-10-19 18:26:58 +0530

    Final After Registraion

--- src/components/after_registration/AfterRegistration.jsx
+++ src/components/after_registration/AfterRegistration.jsx
@@ -13,6 +13,7 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useState } from "react";
+import Popover from "@mui/material/Popover";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
@@ -40,105 +41,118 @@ function Copyright(props) {
 const defaultTheme = createTheme();
 
 export default function AfterRegistration() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
   const [tpu, settpu] = useState("");
-  const handleChange = (e) => {
+  const [maxupto, setmaxupto] = useState("");
+  const [refresh, setrefresh] = useState("");
+  const [details, setDetails] = useState("Hello Owrld");
+  const handleChangetpu = (e) => {
     settpu(e.target.value);
   };
+  const handleChangemaxupto = (e) => {
+    setmaxupto(e.target.value);
+  };
+  const handleChangerefresh = (e) => {
+    setrefresh(e.target.value);
+  };
 
+  const handleClick = () => {
+    setDetails(
+      "event.currentTarget jhadsjkklskdlsa loremsjdnajksdnkajsndjkands"
+    );
+  };
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Pay Per use
-          </Typography>
+    <div>
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
           <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            sx={{ mt: 1 }}
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-            <FormControl sx={{ m: 1, minWidth: 400 }}>
-              <InputLabel id="demo-simple-select-helper-label">
-                Transaction Per Use
-              </InputLabel>
-              <Select
-                labelId="demo-simple-select-helper-label"
-                id="demo-simple-select-helper"
-                value={tpu}
-                label="Transaction Per Use"
-                onChange={handleChange}
+            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Pay Per use
+            </Typography>
+            <Box component="form" noValidate sx={{ mt: 3 }}>
+              <FormControl sx={{ m: 2, minWidth: 400 }}>
+                <InputLabel id="tpu">Transaction Per Use</InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  id="tpu"
+                  value={tpu}
+                  label="Transaction Per Use"
+                  onChange={handleChangetpu}
+                >
+                  <MenuItem value={1}>1 CHF</MenuItem>
+                  <MenuItem value={2}>2 CHF</MenuItem>
+                  <MenuItem value={3}>3 CHF</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl sx={{ m: 2, minWidth: 400 }}>
+                <InputLabel id="maxupto">MaxUpto</InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  value={maxupto}
+                  onChange={handleChangemaxupto}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value={10}>10 CHF</MenuItem>
+                  <MenuItem value={20}>20 CHF</MenuItem>
+                  <MenuItem value={30}>30 CHF</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl sx={{ m: 2, minWidth: 400 }}>
+                <InputLabel id="refresh">Refresh</InputLabel>
+                <Select
+                  labelId="demo-simple-select-helper-label"
+                  value={refresh}
+                  onChange={handleChangerefresh}
+                  displayEmpty
+                  inputProps={{ "aria-label": "Without label" }}
+                >
+                  <MenuItem value={10}>10</MenuItem>
+                  <MenuItem value={20}>20</MenuItem>
+                  <MenuItem value={30}>30</MenuItem>
+                </Select>
+              </FormControl>
+              <Button
+                type="submit"
+                onClick={handleClick}
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
               >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl sx={{ m: 1, minWidth: 400 }}>
-              <Select
-                value={tpu}
-                onChange={handleChange}
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl sx={{ m: 1, minWidth: 400 }}>
-              <Select
-                value={tpu}
-                onChange={handleChange}
-                displayEmpty
-                inputProps={{ "aria-label": "Without label" }}
-              >
-                <MenuItem value="">
-                  <em>None</em>
-                </MenuItem>
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
-              </Select>
-            </FormControl>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              See Details
-            </Button>
+                See Details
+              </Button>
+            </Box>
           </Box>
-        </Box>
-        <Copyright sx={{ mt: 5 }} />
-      </Container>
-    </ThemeProvider>
+          <Copyright sx={{ mt: 5 }} />
+        </Container>
+      </ThemeProvider>
+      <div
+        style={{
+          backgroundColor: "grey",
+          height: "fit-content",
+          textAlign: "center",
+          marginTop: "20px",
+          display: "flex",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {tpu}
+        {refresh}
+        {maxupto}
+        {details}
+      </div>
+    </div>
   );
 }
