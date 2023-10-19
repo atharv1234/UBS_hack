7137eb52b50a9b4525553eeee021a68f6bf74fe6
Harish-Choudhary <harishchoudhary605@gmail.com>
2023-10-19 14:38:17 +0530

    added registerCardWithBanks Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Login from "./components/login/Login"
 import SignUp from "./components/signup/SignUp"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import RegisterCardBank from './components/registerCardBank/RegisterCardBank'
 function App() {
 
   return <div>
@@ -9,6 +10,7 @@ function App() {
       <Routes>
           <Route path="/signup" element={<SignUp />} />
           <Route path="*" element={<Login />} />
+          <Route path="/registerCard" element={<RegisterCardBank/>}></Route>
       </Routes>
     </BrowserRouter>
   </div>;
